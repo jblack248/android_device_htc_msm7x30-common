403bb51865f5bc7380357fb8eb53b567ea1d06d9
Joe Black <jblack248@gmail.com>
2013-03-17 22:43:33 -0400

    Fix product locales to en_US

--- msm7x30.mk
+++ msm7x30.mk
@@ -106,8 +106,7 @@ endif
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 # use high-density artwork where available
-PRODUCT_LOCALES += hdpi
-
+PRODUCT_LOCALES += en_US
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
